a7b823b47f53df1022517b0d1498cb6f8742a5b4
Alex <60221362+aw950309@users.noreply.github.com>
2025-11-14 10:13:23 +0100

    Changed comment language.

--- Challenge1.1.py
+++ Challenge1.1.py
@@ -1,11 +1,11 @@
-# Ciphertext från challenge 1.1
+# Ciphertext from challenge 1.1.
 ciphertext = "D_AZ_5H7S006_9WHF6BHD_33HX_5VHSAH3WS0AHIJHX3SY0H064WH6XHAZW4HS9WHX_3WH5S4WVHX3SYH5HTBAH064WA_4W0HAZWHX3SYH_0HZ_VVW5H_5HS56AZW9HX_3WHAZ_0H4W00SYWH_0HAZWHS50DW9HA6HUZS33W5YWHIHV6AHI"
 
-# Definiera alfabetet som används i ciphern
+# Define the alphabet used in the cipher.
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_1234567890"
 n = len(alphabet)
 
-# Funktion för att dekryptera med en given nyckel
+# Function to decrypt with a given key.
 def decrypt(ciphertext, key):
     plaintext = ''.join(
         alphabet[(alphabet.index(char) + key) % n]
@@ -13,10 +13,10 @@ def decrypt(ciphertext, key):
     )
     return plaintext.replace('_', ' ')
 
-# Generera alla möjliga plaintexts
+# Generate all possible plaintexts.
 possible_plaintexts = [(key, decrypt(ciphertext, key)) for key in range(1, n)]
 
-# Filtrera ut läsbara texter
+# Filter out readable texts.
 readable = [pt for pt in possible_plaintexts if any(word in pt[1] for word in ["THE", "AND", "YOU"])]
 
 print(readable)
